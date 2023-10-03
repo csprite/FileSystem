67e2437469328951e918ea5c6f847d72f648bcec
pegvin <75035219+pegvin@users.noreply.github.com>
2023-10-03 23:40:58 +0530

    Write First Test

--- test/test.cpp
+++ test/test.cpp
@@ -1,14 +1,25 @@
 #include "minunit/minunit.h"
+#include "fs/fs.hpp"
 
-MU_TEST(test_check) {
-	mu_check(5 == 7);
-}
-MU_TEST_SUITE(test_suite) {
-	MU_RUN_TEST(test_check);
+namespace Fs = FileSystem;
+
+MU_TEST(Test_Fs_NormalizePath) {
+#ifdef TEST_TARGET_WINDOWS
+	const char NormalPath[] = "\\aa\\bb\\cc";
+#else
+	const char NormalPath[] = "/aa/bb/cc";
+#endif
+	String Normalized = "/aa\\bb/cc";
+	Fs::NormalizePath(Normalized);
+	mu_check(Normalized == NormalPath);
 }
 
-int main(int argc, char *argv[]) {
-	MU_RUN_SUITE(test_suite);
+MU_TEST_SUITE(MainSuite) {
+	MU_RUN_TEST(Test_Fs_NormalizePath);
+}
+
+int main(void) {
+	MU_RUN_SUITE(MainSuite);
 	MU_REPORT();
 	return MU_EXIT_CODE;
 }
