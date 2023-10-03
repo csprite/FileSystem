14fee0161649253456b897d0fde8a01a224f8217
pegvin <75035219+pegvin@users.noreply.github.com>
2023-10-03 23:51:36 +0530

    Add More Test

--- test/test.cpp
+++ test/test.cpp
@@ -21,9 +21,18 @@ MU_TEST(Test_Fs_GetParentDir) {
 #endif
 }
 
+MU_TEST(Test_Fs_GetBaseName) {
+#ifdef TEST_TARGET_WINDOWS
+	mu_check(Fs::GetBaseName("\\aa\\bb\\cc") == "cc");
+#else
+	mu_check(Fs::GetBaseName("/aa/bb/cc") == "cc");
+#endif
+}
+
 MU_TEST_SUITE(MainSuite) {
 	MU_RUN_TEST(Test_Fs_NormalizePath);
 	MU_RUN_TEST(Test_Fs_GetParentDir);
+	MU_RUN_TEST(Test_Fs_GetBaseName);
 }
 
 int main(void) {
